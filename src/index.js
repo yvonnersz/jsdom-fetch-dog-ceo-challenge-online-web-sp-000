0602b9824dfb7bf54cc48af7d136c360c8bac278
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-20 20:01:01 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ console.log('%c HI', 'color: firebrick')
 // add image elements to the DOM for each🤔 image in the array
 
 document.addEventListener("DOMContentLoaded", () => {
-  // fetchImages();
+  fetchImages();
   fetchDogBreed();
 });
 
@@ -30,6 +30,10 @@ function renderImages(json) {
 
 // Once all of the breeds are rendered in the <ul>, add JavaScript so that the font color of a particular <li> changes on click. This can be a color of your choosing.
 // When the user clicks any of the dog breed list items, the color the text should change.
+
+// Once we are able to load all of the dog breeds onto the page, add JavaScript so that the user can filter breeds that start with a particular letter using a dropdown.
+// For example, if the user selects 'a' in the dropdown, only show the breeds with names that start with the letter a. For simplicity, the dropdown only includes the letters a-d. However, we can imagine expanding this to include the entire alphabet
+
 
 function fetchDogBreed() {
   fetch('https://dog.ceo/api/breeds/list/all')
@@ -64,6 +68,3 @@ function renderDogBreed(json) {
 
   }
 }
-
-// Once we are able to load all of the dog breeds onto the page, add JavaScript so that the user can filter breeds that start with a particular letter using a dropdown.
-// For example, if the user selects 'a' in the dropdown, only show the breeds with names that start with the letter a. For simplicity, the dropdown only includes the letters a-d. However, we can imagine expanding this to include the entire alphabet
